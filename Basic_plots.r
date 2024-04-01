1113ef7d953d991a1c3f236e070a0155b3287fc5
AtulKadlag <62134276+AtulKadlag@users.noreply.github.com>
2024-04-01 11:44:22 +0530

    Add files via upload

---
+++ Basic_plots.r
@@ -0,0 +1,14 @@
+
+plot(1, 3)
+plot(1:10, type = "l")
+plot(1:10, type = "l", col = "red")
+plot(1:10, type ="l", lwd = 2, col = "red")
+
+plot(1:10, type="l", lwd=5, lty=3)
+
+
+line1 <- c(1, 2, 3, 4, 5, 10)
+line2 <- c(2, 5, 7, 8, 9, 10)
+
+plot(line1, type = "l", col = "blue")
+lines(line2, type="l", col = "red")
